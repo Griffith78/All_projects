881a17d9e9db5945e08dfe303d8cf12fb2ca66a9
Griffith <mxyurchenko1@gmail.com>
2025-05-09 21:03:16 +0300

    create bot dictionaries

--- Python_files/test_project.py
+++ Python_files/test_project.py
@@ -1,5 +1,82 @@
 import telebot
+import sqlite3
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = '7863780400:AAHjL1GRqD19PKAsV-Sefy20doMrls0z6RY'
+bot = telebot.TeleBot('7863780400:AAHjL1GRqD19PKAsV-Sefy20doMrls0z6RY')
 
-@bot.message
+word = ''
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    conn = sqlite3.connect('Test.sql')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS dictionaries (id INTEGER PRIMARY KEY AUTOINCREMENT,word varchar(50), translation varchar(50) )")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton('Add', callback_data = 'Add'))
+    markup.add(InlineKeyboardButton('Find', callback_data = 'Find'))
+    markup.add(InlineKeyboardButton('Print', callback_data = 'Print'))
+    bot.send_message(message.chat.id,'Hello,my friend. It is dictionary',reply_markup = markup)
+
+@bot.callback_query_handler(func = lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'Add':
+       bot.send_message(callback.message.chat.id,'Good.Write word')
+       bot.register_next_step_handler(callback.message,register_word)
+
+    if callback.data == 'Find':
+        bot.send_message(callback.message.chat.id,'write word')
+        bot.register_next_step_handler(callback.message,find_word)
+
+    if callback.data == 'Print':
+        print(callback.message)
+
+def print(message):
+    conn = sqlite3.connect('Test.sql')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM dictionaries")
+    query = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    all_words = ''
+    for i in query:
+        all_words += f'• ID : {i[0]} Word: {i[1]} Translation: {i[2]} \n'
+    bot.send_message(message.chat.id ,all_words)
+
+def find_word(message):
+    try:
+        search_word = message.text.strip()
+        conn = sqlite3.connect('Test.sql')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM dictionaries WHERE word = '%s'" %(search_word))
+        query_result = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        result = f'ID: {query_result[0]} Word: {query_result[1]} Translation: {query_result[2]}'
+        bot.send_message(message.chat.id,result)
+    except TypeError:
+        bot.send_message(message.chat.id,'The word was not found or the input was incorrect ')
+
+def register_word(message):
+    global word
+    word = message.text.strip()
+    bot.send_message(message.chat.id,'Now,write translation')
+    bot.register_next_step_handler(message,register_translation )
+
+def register_translation(message):
+    global word
+    translation =  message.text.strip()
+    conn = sqlite3.connect('Test.sql')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO dictionaries(word,translation) VALUES ('%s','%s' )" %(word,translation))
+    conn.commit()
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id,'a')
+
+bot.polling(none_stop = True)
